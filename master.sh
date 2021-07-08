2cee93fe684315db77d3c0d91896ce92c448e119
jroemer551 <82119662+jroemer551@users.noreply.github.com>
2021-07-08 06:04:22 +0000

    Add files via upload

---
+++ master.sh
@@ -0,0 +1,10 @@
+echo 'deb https://assets.checkra.in/debian /' | sudo tee /etc/apt/sources.list.d/checkra1n.list
+sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
+sudo apt-get update
+sudo apt-get install checkra1n -y
+sudo apt install gparted -y
+sudo apt install curl -y
+sudo apt install git -y
+sudo apt install  ssh  -y
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
